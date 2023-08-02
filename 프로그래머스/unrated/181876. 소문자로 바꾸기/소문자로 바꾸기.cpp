77ba0c81b70457dcef7f986492921a1973bc1284
최진원 <cjw4767@naver.com>
2023-08-02 20:14:15 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(int i = 0; i < myString.length(); i++)
+        answer += tolower(myString[i]);
+    
+    return answer;
+}

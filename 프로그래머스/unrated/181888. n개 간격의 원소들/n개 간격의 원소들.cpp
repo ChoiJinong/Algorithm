b9ffb09d43d54983e66e34a7bceff16ebcba7490
최진원 <cjw4767@naver.com>
2023-08-07 16:56:38 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    for (int i = 0; i < num_list.size(); i++) {
+		if (i * n < num_list.size())
+			answer.push_back(num_list[i * n]);
+	}
+    return answer;
+}
